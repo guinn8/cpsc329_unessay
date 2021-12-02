956f9b8a6a0f92d7f4ff5c998ac529afe17f9521
Gavin Guinn <gavinguinn1@gmail.com>
2021-12-02 15:08:43 -0700

    finalized game loop?

--- quiz.py
+++ quiz.py
@@ -1,14 +1,20 @@
 import curses
 import enum
 import json
+import random
  
 class quiz_state(enum.Enum):
     TITLE = 1
     QUESTION = 2
     END = 3
+    QUESTION_ANS = 4
 
 QUESTIONS_TO_ASK = 7
-ENTER = 10
+ASCII_ENTER = 10
+ASCII_1 = 49
+ASCII_2 = 50
+ASCII_3 = 51
+ASCII_4 = 52
 
 # Boiler plate from:
 # https://gist.github.com/claymcleod/b670285f334acd56ad1c
@@ -18,15 +24,16 @@ def main(stdscr):
     global question_counter
 
     f = open("questions.json", "r")
-    q_list = json.loads(f.read())
+    all_quest_list = json.loads(f.read())
+    q_list = random.sample(all_quest_list, 7)
 
-    # Start colors in curses
     curses.start_color()
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.curs_set(0)
 
+    ans = -1
     k = 0
     question_counter = 0
     state = quiz_state.TITLE
@@ -35,17 +42,31 @@ def main(stdscr):
         stdscr.clear()
 
         if(state == quiz_state.TITLE):
-            if(k == ENTER):
+            if(k == ASCII_ENTER):
                 state = quiz_state.QUESTION
                 continue
-            draw_title(stdscr)
+            else:
+                draw_title(stdscr)
 
         elif(state == quiz_state.QUESTION):
-            draw_question(stdscr, q_list[0])
-            question_counter += 1
-            if(question_counter > QUESTIONS_TO_ASK):
-                state = quiz_state.END
+            if (ASCII_1 <= k <= ASCII_4):
+                state = quiz_state.QUESTION_ANS
+                ans = k - ASCII_1
                 continue
+            else:
+                draw_question(stdscr, q_list[question_counter])
+
+        elif(state == quiz_state.QUESTION_ANS):
+            assert(ans != -1)
+            if(k == ASCII_ENTER): 
+                question_counter += 1
+                if(question_counter == QUESTIONS_TO_ASK):
+                    state = quiz_state.END
+                else:
+                    state = quiz_state.QUESTION
+                continue
+            else:
+                draw_question_followup(stdscr, q_list[question_counter], ans)
 
         elif(state == quiz_state.END):
             draw_end(stdscr)
@@ -58,9 +79,9 @@ def draw_status(stdscr):
     statusbarstr = "Press 'q' to exit"
     quiz_sts_string = ""
     if(question_counter > 0 and question_counter <= QUESTIONS_TO_ASK):
-        quiz_sts_string = " | {} /  {}  Questions in quiz".format(question_counter, QUESTIONS_TO_ASK)
+        quiz_sts_string = " | {} / {} Questions in quiz".format(question_counter, QUESTIONS_TO_ASK)
 
-    statusbarstr += quiz_sts_string
+    statusbarstr += quiz_sts_string + " | Use keys '1' - '4' to select an answer"
     status_bar_pad = " " * (width - len(statusbarstr) - 1)
     
     stdscr.attron(curses.color_pair(3))
@@ -77,10 +98,25 @@ def draw_end(stdscr):
 
 def draw_question(stdscr, q):
     title = q["ask"]
-    start_y = int((height // 2) - 2)
+    start_y = 1
     start_x_title = calc_center_text(title)
-
     stdscr.addstr(start_y, start_x_title, title, curses.color_pair(2) | curses.A_BOLD)
+
+    for i in range(0, 4):
+        choice = "{}) {}".format(i+1, q["choices"][i])
+        start_x_choice = calc_center_text(choice)
+        stdscr.addstr(start_y + (2 * (i + 1)), start_x_choice, choice)
+
+def draw_question_followup(stdscr, q, ans):
+    title = "followup" + str(ans) #q["ask"]
+    start_y = 1
+    start_x_title = calc_center_text(title)
+    stdscr.addstr(start_y, start_x_title, title, curses.color_pair(2) | curses.A_BOLD)
+
+    for i in range(0, 4):
+        choice = "{}) {}".format(i+1, q["choices"][i])
+        start_x_choice = calc_center_text(choice)
+        stdscr.addstr(start_y + (2 * (i + 1)), start_x_choice, choice)
 
 def draw_title(stdscr):
     title = "Curses example"[:width-1]
