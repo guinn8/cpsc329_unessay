4e0db5a903421989661fe9d1354ce721a9fc39d4
Gavin Guinn <gavinguinn1@gmail.com>
2021-12-02 03:49:39 -0700

    basic game loop implemented

--- quiz.py
+++ quiz.py
@@ -1,94 +1,110 @@
-import sys,os
 import curses
+import enum
+ 
+class quiz_state(enum.Enum):
+    TITLE = 1
+    QUESTION = 2
+    END = 3
+
+QUESTIONS_TO_ASK = 7
+ENTER = 10
 
 # Boiler plate from:
 # https://gist.github.com/claymcleod/b670285f334acd56ad1c
-def draw_menu(stdscr):
-    k = 0
-    cursor_x = 0
-    cursor_y = 0
-
-    # Clear and refresh the screen for a blank canvas
-    stdscr.clear()
-    stdscr.refresh()
+def main(stdscr):
+    global height
+    global width
+    global question_counter
+    question_counter = 0
 
     # Start colors in curses
     curses.start_color()
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.curs_set(0)
 
-    # Loop where k is the last character pressed
+    k = 0
+    state = quiz_state.TITLE
     while (k != ord('q')):
-
-        # Initialization
-        stdscr.clear()
         height, width = stdscr.getmaxyx()
+        stdscr.clear()
 
-        if k == curses.KEY_DOWN:
-            cursor_y = cursor_y + 1
-        elif k == curses.KEY_UP:
-            cursor_y = cursor_y - 1
-        elif k == curses.KEY_RIGHT:
-            cursor_x = cursor_x + 1
-        elif k == curses.KEY_LEFT:
-            cursor_x = cursor_x - 1
+        if(state == quiz_state.TITLE):
+            if(k == ENTER):
+                state = quiz_state.QUESTION
+                continue
+            draw_title(stdscr)
 
-        cursor_x = max(0, cursor_x)
-        cursor_x = min(width-1, cursor_x)
+        elif(state == quiz_state.QUESTION):
+            draw_question(stdscr)
+            question_counter += 1
+            if(question_counter > QUESTIONS_TO_ASK):
+                state = quiz_state.END
+                continue
 
-        cursor_y = max(0, cursor_y)
-        cursor_y = min(height-1, cursor_y)
-
-        # Declaration of strings
-        title = "Curses example"[:width-1]
-        subtitle = "Written by Clay McLeod"[:width-1]
-        keystr = "Last key pressed: {}".format(k)[:width-1]
-        statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
-        if k == 0:
-            keystr = "No key press detected..."[:width-1]
-
-        # Centering calculations
-        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
-        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
-        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
-        start_y = int((height // 2) - 2)
-
-        # Rendering some text
-        whstr = "Width: {}, Height: {}".format(width, height)
-        stdscr.addstr(0, 0, whstr, curses.color_pair(1))
-
-        # Render status bar
-        stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(height-1, 0, statusbarstr)
-        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-        stdscr.attroff(curses.color_pair(3))
-
-        # Turning on attributes for title
-        stdscr.attron(curses.color_pair(2))
-        stdscr.attron(curses.A_BOLD)
-
-        # Rendering title
-        stdscr.addstr(start_y, start_x_title, title)
-
-        # Turning off attributes for title
-        stdscr.attroff(curses.color_pair(2))
-        stdscr.attroff(curses.A_BOLD)
-
-        # Print rest of text
-        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
-        stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
-        stdscr.addstr(start_y + 5, start_x_keystr, keystr)
-        stdscr.move(cursor_y, cursor_x)
-
-        # Refresh the screen
+        elif(state == quiz_state.END):
+            draw_end(stdscr)
+        
+        draw_status(stdscr)
         stdscr.refresh()
-
-        # Wait for next input
         k = stdscr.getch()
 
-def main():
-    curses.wrapper(draw_menu)
+def draw_status(stdscr):
+    statusbarstr = "Press 'q' to exit"
+    quiz_sts_string = ""
+    if(question_counter > 0 and question_counter <= QUESTIONS_TO_ASK):
+        quiz_sts_string = " | {} /  {}  Questions in quiz".format(question_counter, QUESTIONS_TO_ASK)
+
+    statusbarstr += quiz_sts_string
+
+    stdscr.attron(curses.color_pair(3))
+    stdscr.addstr(height-1, 0, statusbarstr)
+    stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+    stdscr.attroff(curses.color_pair(3))
+
+def draw_end(stdscr):
+    title = "FIN"
+    start_y = int((height // 2) - 2)
+    start_x_title = calc_center_text(title)
+
+    stdscr.attron(curses.color_pair(1))
+    stdscr.attron(curses.A_BOLD)
+    stdscr.addstr(start_y, start_x_title, title)
+    stdscr.attroff(curses.color_pair(1))
+    stdscr.attroff(curses.A_BOLD)
+
+def draw_question(stdscr):
+    title = "THIS IS A QUESTION!!!!"
+    start_y = int((height // 2) - 2)
+    start_x_title = calc_center_text(title)
+
+    stdscr.attron(curses.color_pair(2))
+    stdscr.attron(curses.A_BOLD)
+    stdscr.addstr(start_y, start_x_title, title)
+    stdscr.attroff(curses.color_pair(2))
+    stdscr.attroff(curses.A_BOLD)
+
+def draw_title(stdscr):
+    title = "Curses example"[:width-1]
+    subtitle = "Written by Clay McLeod"[:width-1]
+
+    start_x_title = calc_center_text(title)
+    start_y = int((height // 2) - 2)
+
+    stdscr.attron(curses.color_pair(2))
+    stdscr.attron(curses.A_BOLD)
+    stdscr.addstr(start_y, start_x_title, title)
+    stdscr.attroff(curses.color_pair(2))
+    stdscr.attroff(curses.A_BOLD)
+
+    start_x_subtitle = calc_center_text(subtitle)
+    stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
+    stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
+
+def calc_center_text(str):
+    return int((width // 2) - (len(str) // 2) - len(str) % 2)
 
 if __name__ == "__main__":
-    main()
+    curses.wrapper(main)
+    
