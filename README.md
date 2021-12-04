8fb52b9abeec406b75fbad6b9e31ac369003c4e0
Gavin Guinn <gavinguinn1@gmail.com>
2021-12-03 19:32:33 -0700

    ready for submission.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cpsc329_unessay
-Quiz game for CPSC 329 Unessay Project
+
+Quiz game for CPSC329 Unessay, simply run quiz.py to play. The program doesn't require any extra packages so it should be highly portable. Tested on Ubuntu 20.
--- quiz.py
+++ quiz.py
@@ -2,6 +2,8 @@ import curses
 import enum
 import json
 import random
+import textwrap
+import sys
  
 class quiz_state(enum.Enum):
     TITLE = 1
@@ -16,12 +18,29 @@ ASCII_2 = 50
 ASCII_3 = 51
 ASCII_4 = 52
 
+art = """
+                 __                              _________ 
+..-'''-.    ...-'  |`. ..-'''-.   ..-'''-.      /         |
+\.-'''\ \   |      |  |\.-'''\ \  \.-'''\ \    '-----.   .'
+       | |  ....   |  |       | |        | |       .'  .'  
+    __/ /     -|   |  |    __/ /      __/ /      .'  .'    
+   |_  '.      |   |  |   |_  '.     |_  '.    .'  .'      
+      `.  \ ...'   `--'      `.  \      `.  \ '---'        
+        \ '.|         |`.      \ '.       \ '.             
+         , |` --------\ |       , |        , |             
+         | | `---------'        | |        | |             
+        / ,'                   / ,'       / ,'             
+-....--'  /            -....--'  /-....--'  /              
+`.. __..-'             `.. __..-' `.. __..-'               
+"""
+
 # Boiler plate from:
 # https://gist.github.com/claymcleod/b670285f334acd56ad1c
 def main(stdscr):
     global height
     global width
     global question_counter
+    question_counter = 0
 
     f = open("questions.json", "r")
     all_quest_list = json.loads(f.read())
@@ -31,11 +50,13 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.curs_set(0)
 
     ans = -1
     k = 0
-    question_counter = 0
+    score = 0
     state = quiz_state.TITLE
     while (k != ord('q')):
         height, width = stdscr.getmaxyx()
@@ -59,7 +80,10 @@ def main(stdscr):
         elif(state == quiz_state.QUESTION_ANS):
             assert(ans != -1)
             if(k == ASCII_ENTER): 
+                if (ans == q_list[question_counter]["correct"]):
+                    score += 1
                 question_counter += 1
+
                 if(question_counter == QUESTIONS_TO_ASK):
                     state = quiz_state.END
                 else:
@@ -69,19 +93,24 @@ def main(stdscr):
                 draw_question_followup(stdscr, q_list[question_counter], ans)
 
         elif(state == quiz_state.END):
-            draw_end(stdscr)
+            draw_end(stdscr, score)
         
-        draw_status(stdscr)
-        stdscr.refresh()
+        draw_status(stdscr, state)
+       
         k = stdscr.getch()
+        stdscr.refresh()
 
-def draw_status(stdscr):
-    statusbarstr = "Press 'q' to exit"
+def draw_status(stdscr, state):
+    statusbarstr = " Press 'q' to exit"
     quiz_sts_string = ""
-    if(question_counter > 0 and question_counter <= QUESTIONS_TO_ASK):
-        quiz_sts_string = " | {} / {} Questions in quiz".format(question_counter, QUESTIONS_TO_ASK)
+    usage_str = ""
+    if(state == quiz_state.QUESTION):
+        quiz_sts_string = " | {} / {} Questions in quiz".format(question_counter + 1, QUESTIONS_TO_ASK)
+        usage_str = " | Use keys '1' - '4' to select an answer"
+    elif(state == quiz_state.QUESTION_ANS or state == quiz_state.TITLE):
+        usage_str = " | Press 'Enter' to continue"
 
-    statusbarstr += quiz_sts_string + " | Use keys '1' - '4' to select an answer"
+    statusbarstr += quiz_sts_string + usage_str
     status_bar_pad = " " * (width - len(statusbarstr) - 1)
     
     stdscr.attron(curses.color_pair(3))
@@ -89,51 +118,94 @@ def draw_status(stdscr):
     stdscr.addstr(height-1, len(statusbarstr), status_bar_pad)
     stdscr.attroff(curses.color_pair(3))
 
-def draw_end(stdscr):
+def draw_end(stdscr, score):
     title = "FIN"
     start_y = int((height // 2) - 2)
-    start_x_title = calc_center_text(title)
+    start_x_title = calc_center_text(len(title))
 
     stdscr.addstr(start_y, start_x_title, title, curses.color_pair(1) | curses.A_BOLD)
+
+    insult = ""
+    if(score <= 2):
+        insult = "U have b33n pwned scrub"
+    elif(score <= 4):
+        insult = "Wat are some sorta scr1pt k1ddy"
+    elif(score <= 6):
+        insult = "Ok you are almost 31337"
+    elif(score == 7):
+        insult = "We have a U8B3R 31337 H4XX0R"
+    score_str = " {}, your score is {} / {} ".format(insult, score, QUESTIONS_TO_ASK)
+    score_str_x = calc_center_text(len(score_str))
+    stdscr.addstr(start_y + 2, score_str_x, score_str, curses.color_pair(5) | curses.A_BOLD)
+
 
 def draw_question(stdscr, q):
     title = q["ask"]
     start_y = 1
-    start_x_title = calc_center_text(title)
+    start_x_title = calc_center_text(len(title))
     stdscr.addstr(start_y, start_x_title, title, curses.color_pair(2) | curses.A_BOLD)
 
+    start_x_choice = calc_center_text(len(q["choices"][0]))
     for i in range(0, 4):
         choice = "{}) {}".format(i+1, q["choices"][i])
-        start_x_choice = calc_center_text(choice)
         stdscr.addstr(start_y + (2 * (i + 1)), start_x_choice, choice)
 
 def draw_question_followup(stdscr, q, ans):
-    title = "followup" + str(ans) #q["ask"]
+    title =  q["ask"]
     start_y = 1
-    start_x_title = calc_center_text(title)
+    start_x_title = calc_center_text(len(title))
     stdscr.addstr(start_y, start_x_title, title, curses.color_pair(2) | curses.A_BOLD)
 
+    start_x_choice = calc_center_text(len(q["choices"][0]))
     for i in range(0, 4):
-        choice = "{}) {}".format(i+1, q["choices"][i])
-        start_x_choice = calc_center_text(choice)
-        stdscr.addstr(start_y + (2 * (i + 1)), start_x_choice, choice)
+        highlight = 0
+        highlight_string = ""
+        if(i == ans):
+            highlight = curses.color_pair(5) # red
+            highlight_string = " <-- Your response"
+        if(i == q["correct"]):
+            highlight = curses.color_pair(4) # green
+            highlight_string = " <-- Correct"
+        choice = "{}) {}{}".format(i+1, q["choices"][i], highlight_string)
+        stdscr.addstr(start_y + 2 * (i + 1), start_x_choice, choice, highlight)
+
+    box1_cols = 110
+    box1_rows = 8
+    box1_y = start_y + 2 * (4 + 1)
+    box1_x = calc_center_text(box1_cols)
+    box1 = stdscr.subwin(box1_rows, box1_cols, box1_y, box1_x)
+    box1.addstr(0, 0, textwrap.fill(q["explain"], box1_cols - 1)) #  box1_wid - 1 fixes some weird line breaking
+    box1.immedok(True)
+
 
 def draw_title(stdscr):
-    title = "Curses example"[:width-1]
-    subtitle = "Written by Clay McLeod"[:width-1]
+    title_str = "So you think you're..."
+    title_str_y = 5
+    title_str_x = calc_center_text(len(art.splitlines()[1]))
+    stdscr.addstr(title_str_y, title_str_x, title_str, curses.A_BOLD)
+    rainbow_magic = curses.COLOR_RED
 
-    start_x_title = calc_center_text(title)
-    start_y = int((height // 2) - 2)
+    art_lines = title_str_y
+    for y, line in enumerate(art.splitlines(), 0):
+        start_x_title = calc_center_text(len(line))
+        curses.init_pair(10 + rainbow_magic, rainbow_magic, curses.COLOR_BLACK)
+        stdscr.addstr(title_str_y + y, start_x_title, line, curses.color_pair(10 + rainbow_magic))
+        rainbow_magic += 1
+        if (rainbow_magic > curses.COLOR_WHITE):
+            rainbow_magic = curses.COLOR_RED
+        art_lines += 1
 
-    stdscr.addstr(start_y, start_x_title, title, curses.color_pair(2) | curses.A_UNDERLINE)
+    subtitle = "A quiz game designed to test your privacy and security skills!"
+    start_x_subtitle = calc_center_text(len(subtitle))
+    stdscr.addstr(art_lines + 1, start_x_subtitle, subtitle, curses.A_BOLD)
 
-    start_x_subtitle = calc_center_text(subtitle)
-    stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
-    stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
-
-def calc_center_text(str):
-    return int((width // 2) - (len(str) // 2) - len(str) % 2)
+def calc_center_text(strlen):
+    return int((width // 2) - (strlen // 2) - strlen % 2)
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    try:
+        curses.wrapper(main)
+    except curses.error:
+        print("Curses threw an error, try increasing your terminal size.") 
+
     
